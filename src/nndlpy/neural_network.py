95e81e9ff878976f6bb110ccdd32884118c5fc2c
raffaeledanna <rafdanna28@gmail.com>
2024-07-03 20:49:15 +0200

    definizione dei metodi base per la creazione della rete neurale

---
+++ src/nndlpy/neural_network.py
@@ -0,0 +1,20 @@
+class NeuralNetwork:
+    def __init__(self, layer_sizes, activation_functions):
+        # Inizializzazione della rete neurale con strati e funzioni di attivazione
+        pass
+
+    def initialize_weights_bias(self, input_layer_size, output_layer_size):
+        # Implementazione dei pesi e del bias per tutti gli strati della rete
+        pass
+
+    def forward_propagation(self, input_data):
+        # Implementazione della propagazione in avanti
+        pass
+
+    def gradient_descent(self, learning_rate, weights):
+        # Implementazione dell'aggiornamento dei pesi e dei bias utilizzando la discesa del gradiente.
+        pass
+
+    def back_propagation(self, target):
+        # Implementazione della backpropagation
+        pass
