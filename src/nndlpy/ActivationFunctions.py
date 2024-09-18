97a9b4866381daacf93f2b4558c8640ab2bd23f8
giuseppe <giuseppecicchella28@gmail.com>
2024-09-18 16:29:07 +0200

    relu e tanh

--- src/nndlpy/ActivationFunctions.py
+++ src/nndlpy/ActivationFunctions.py
@@ -16,3 +16,36 @@ def sigmoid(x, der=False):
     else:
         sig = 1 / (1 + np.exp(-x))
         return sig * (1 - sig)  # Derivata della funzione sigmoide
+
+def relu(x, der=0):
+    """
+    Calcola la funzione ReLU o la sua derivata.
+
+    Args:
+        x (float): Il valore di input.
+        der (bool, optional): Se è False, calcola la funzione ReLU, altrimenti calcola la derivata. Default è False.
+
+    Returns:
+        float: Se der=False, restituisce il valore della funzione ReLU di x, altrimenti restituisce la sua derivata.
+    """
+    if not der:
+        return np.maximum(0, x)
+    else:
+        return np.where(x > 0, 1, 0)  # Derivata della funzione ReLU
+
+def tanh(x, der=False):
+    """
+    Calcola la tangente iperbolica o anche la sua derivata.
+
+    Args:
+        x (float): Il valore di input.
+        der (bool, optional): Se è False, calcola la tangente iperbolica, altrimenti calcola la derivata. Default è False.
+
+    Returns:
+        float: Se der=False, restituisce il valore della tangente iperbolica di x, altrimenti restituisce la sua derivata.
+    """
+    y = np.tanh(x)
+    if not der:
+        return y
+    else:
+        return 1 - y**2  # Derivata della tangente iperbolica
